3df85a86b040566e77d1df4cd67c4439c3cd11df
xianghexiaoqu <deffyc@139.com>
2016-12-14 00:27:39 +0800

    upload

---
+++ themes/icarus/scripts/lazyload.js
@@ -0,0 +1,25 @@
+'use strict';
+var cheerio = require('cheerio');
+var cdnUrl = "https://youcdndomain.com";
+var loading = "/images/loading.gif";
+var oldsrc = '';
+function stringStartsWith(string, prefix) {
+    return string.slice(0, prefix.length) == prefix;
+}
+function lazyloadImg(source) {
+    var $ = cheerio.load(source, {
+        decodeEntities: false
+    });
+    $('img').each(function(index, element) {
+        oldsrc = $(element).attr('src');
+        if (oldsrc && !stringStartsWith(oldsrc, baseUrl) && !$(element).hasClass('hx_lazyimg') && !$(element).hasClass('skip')) {
+            $(element).addClass('hx_lazyimg');
+            $(element).attr({
+                src: loading,
+                'data-original': cdnUrl + oldsrc
+            });
+        }
+    });
+    return $.html();
+}
+hexo.extend.filter.register('after_render:html', lazyloadImg);
